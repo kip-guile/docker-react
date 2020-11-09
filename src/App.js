b70ed999afdd243b6f623465c4d64e8406661b46
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-11-09 13:49:56 +0100

    force rebuild

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Alright, be professional!
+          Alright, be professional.
         </p>
         <a
           className="App-link"
